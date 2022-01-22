862c46bbb795bb33b07233b6989294020c6ddb63
Mohamed Ramzy <ramzymohamed790@hotmail.com>
2022-01-22 23:35:11 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+  './pages/**/*.{html,js}',
+  './index.html',],
+  theme: {
+    extend: {},
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '975px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    // require('@tailwindcss/aspect-ratio'),
+  ],
+}
